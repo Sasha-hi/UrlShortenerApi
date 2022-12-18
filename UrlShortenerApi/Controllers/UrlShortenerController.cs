95da319d389e86c35183c4ba8febe0fab5aae8e4
aleksandr.h <aleksandr.h@youcontrol.com.ua>
2022-12-18 23:24:43 +0200

    wip

---
+++ UrlShortenerApi/Controllers/UrlShortenerController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UrlShortenerApi.Controllers;
+
+[ApiController]
+[Route("api")]
+public class UrlShortenerController : ControllerBase
+{
+    [HttpGet("")]
+    public string Index()
+    {
+        return "Service available";
+    }
+
+    [HttpGet("{key}")]
+    [ProducesResponseType(StatusCodes.Status302Found)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public IActionResult Expand(string key)
+    {
+        if (string.IsNullOrWhiteSpace(key))
+            return BadRequest($"Argument {nameof(key)} is null or empty or whitespace");
+
+        return Redirect("https://google.com");
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public ActionResult<string> Shorten(string sourceUrl)
+    {
+        if (string.IsNullOrWhiteSpace(sourceUrl))
+            return BadRequest($"Argument {nameof(sourceUrl)} is null or empty or whitespace");
+
+        var routeValues = new { key = "key" };
+        return CreatedAtAction(nameof(Expand), routeValues, "result url");
+    }
+}
